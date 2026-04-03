9f0c69e8b2d47368f328aa84f81efbf800cf82ee
Yozi Heru Maulana <zirocraftid@gmail.com>
2026-04-03 13:42:00 +0700

    chore: initial industrial backend engine with secure env setup

---
+++ src/main/java/com/zirocraft/zirologistics/ZirologisticsApplication.java
@@ -0,0 +1,13 @@
+package com.zirocraft.zirologistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZirologisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZirologisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zirocraft/zirologistics/ZirologisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zirocraft.zirologistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZirologisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
