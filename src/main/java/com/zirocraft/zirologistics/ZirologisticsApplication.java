0b63f59a167ec2d8ea95b1ed2286b4166a709125
Yozi Heru Maulana <zirocraftid@gmail.com>
2026-04-03 14:42:59 +0700

    feat: implement core warehouse architecture and enable JPA auditing 🛡️⚙️

--- src/main/java/com/zirocraft/zirologistics/ZirologisticsApplication.java
+++ src/main/java/com/zirocraft/zirologistics/ZirologisticsApplication.java
@@ -2,8 +2,10 @@ package com.zirocraft.zirologistics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing; // Tambahkan ini
 
 @SpringBootApplication
+@EnableJpaAuditing // SANGAT PENTING: Untuk mengaktifkan fitur Audit Trail
 public class ZirologisticsApplication {
 
 	public static void main(String[] args) {
