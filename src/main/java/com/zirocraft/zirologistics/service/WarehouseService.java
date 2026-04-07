3ceec211e6c2964515d90921c3d7838a8887105c
Yozi Heru Maulana <zirocraftid@gmail.com>
2026-04-07 17:49:39 +0700

    refactor: update WarehouseService interface to support industrial pagination

--- src/main/java/com/zirocraft/zirologistics/service/WarehouseService.java
+++ src/main/java/com/zirocraft/zirologistics/service/WarehouseService.java
@@ -2,9 +2,12 @@ package com.zirocraft.zirologistics.service;
 
 import com.zirocraft.zirologistics.io.request.WarehouseRequest;
 import com.zirocraft.zirologistics.io.response.WarehouseResponse;
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface WarehouseService {
     WarehouseResponse createWarehouse(WarehouseRequest request);
-    List<WarehouseResponse> getAllWarehouses();
+
+    // Industrial Standard: Wajib pakai Pageable
+    Page<WarehouseResponse> getAllWarehouses(Pageable pageable);
 }
