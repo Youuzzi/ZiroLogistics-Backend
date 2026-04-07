ddcc7520f4daf42b5131a4775903f67da5b44bde
Yozi Heru Maulana <zirocraftid@gmail.com>
2026-04-07 17:47:49 +0700

    feat: add paginated finder for active warehouses in WarehouseRepository

--- src/main/java/com/zirocraft/zirologistics/repository/WarehouseRepository.java
+++ src/main/java/com/zirocraft/zirologistics/repository/WarehouseRepository.java
@@ -1,10 +1,15 @@
 package com.zirocraft.zirologistics.repository;
 
 import com.zirocraft.zirologistics.entity.WarehouseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface WarehouseRepository extends JpaRepository<WarehouseEntity, Long> {
     Optional<WarehouseEntity> findByPublicId(String publicId);
     Optional<WarehouseEntity> findByCode(String code);
+
+    // Method untuk ambil data yang belum dihapus dengan fitur Pagination
+    Page<WarehouseEntity> findAllByIsDeletedFalse(Pageable pageable);
 }
