22c22261326aefda00cd8796d12c8a8f77e3f4f2
Yozi Heru Maulana <zirocraftid@gmail.com>
2026-04-07 17:47:06 +0700

    refactor: implement paginated transaction history in InventoryLedgerRepository

--- src/main/java/com/zirocraft/zirologistics/repository/InventoryLedgerRepository.java
+++ src/main/java/com/zirocraft/zirologistics/repository/InventoryLedgerRepository.java
@@ -1,9 +1,17 @@
 package com.zirocraft.zirologistics.repository;
 
 import com.zirocraft.zirologistics.entity.InventoryLedgerEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InventoryLedgerRepository extends JpaRepository<InventoryLedgerEntity, Long> {
-    // Untuk cek Idempotency (Apakah request ID sudah pernah diproses?)
+
+    // Method untuk mengecek Idempotency
     boolean existsByRequestId(String requestId);
+
+    // Industrial Standard: Ambil data riwayat ter-update dengan Pagination
+    Page<InventoryLedgerEntity> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
